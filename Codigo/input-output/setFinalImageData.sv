0b5caa7af7e9f07b343e96a6dcca165618b44f7a
EmiSancho <emilysancho@gmail.com>
2022-05-11 22:24:23 -0600

    Instruction memory and TB for Fecth Stage

--- Codigo/input-output/setFinalImageData.sv
+++ Codigo/input-output/setFinalImageData.sv
@@ -7,16 +7,17 @@
 */
 
 module setFinalImageData(input logic CLK, WE, 
-						input [7:0] wA,
+						input [17:0] wA,
 						input [7:0] WD
 						);
 						
-	logic [7:0] memory [7:0];	
-
+	logic [7:0] memory [159999:0];	
+	logic [17:0] wx = 18'b000000000111111111;
+	
 	always_ff @(posedge CLK) 
 	begin
 		if(WE)	
-		memory[wA[7:0]] <= WD;
+		memory[wx[7:0]] <= WD;
 		$writememh("C:/Users/ACER/Desktop/Arqui IS 2022/ProyectoGrupal1/grupal1/Codigo/input-output/finalImage.img", memory);
 	end
 	
